f451630037163c3dfcb83fec5e2b2a28e331040d
vasim katiya <vasimkatiya97@gmail.com>
2026-02-28 20:59:15 +0530

    basic setup is completed

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/",(req,res)=>{res.send("<h1>hello world !</h1>")});
+
+app.listen(3000,()=>{
+    console.log(`server is listening !`);
+});
